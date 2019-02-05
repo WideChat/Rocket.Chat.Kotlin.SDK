08216ab935058802df30d9c34aea4710c88775ce
Utkarsh Barsaiyan <ubarsaiyan@gmail.com>
2019-02-05 15:25:45 +0530

    Add _id to queryContactResult

--- core/src/main/kotlin/chat/rocket/core/model/QueryContactResult.kt
+++ core/src/main/kotlin/chat/rocket/core/model/QueryContactResult.kt
@@ -1,9 +1,11 @@
 package chat.rocket.core.model
 
+import com.squareup.moshi.Json
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
 data class ContactHolder(
+        @Json(name = "_id") val id: String,
         val u: String,
         val h: String
 )
